b93e474287dfba6db6969d45427a5efc1bd2e4eb
liao <liaomovie2@gmail.com>
2018-11-11 21:11:15 +0800

    make message more comprehesive

--- write_post.sh
+++ write_post.sh
@@ -4,13 +4,15 @@
 curl --fail "https://raw.githubusercontent.com/Rbloggers/web/posts/posts.tar.gz" > old.tar.gz 
 tar -zxvf old.tar.gz  
 ## Copy old posts to web/
-cp -r _posts/* web/_posts/ && rm -r _posts/ 
+cp -r _posts/* web/_posts/
 
 
 # write new post
+## To Do: 
+##   1. write yaml and file name (author, date): _EOF_, rand_generator
+##   2. insert HTML + insert <!--more-->
 
-
-# 
+# Archive All posts
 cd web
 tar -zcvf posts.tar.gz _posts # Archive _posts
 mv posts.tar.gz ../
@@ -19,11 +21,18 @@ cd -
 # Compare size of old.tar.gz & posts.tar.gz
 new=$(stat --printf="%s" posts.tar.gz)
 old=$(stat --printf="%s" old.tar.gz)
-if [[ $old -ge $new ]]; then 
-    echo 'old.tar.gz is larger than(equal) posts.tar.gz'
+if [[ $old -gt $new ]]; then 
+    echo 'old.tar.gz is larger than posts.tar.gz (Need checking)'
     echo 'Use old.tar.gz instead'
     rm posts.tar.gz
     mv old.tar.gz posts.tar.gz
+elif [[ $old -eq $new ]]; then
+    echo 'old.tar.gz is equal to posts.tar.gz'
+    echo 'Use old.tar.gz instead'
+    rm posts.tar.gz
+    mv old.tar.gz posts.tar.gz
+else
+    echo 'Use posts.tar.gz'
 fi
 
 # Move posts.tar.gz to posts_tar/
